0d8d4445ff53d599ff68a246e55f353c82188ba0
zhaziKemelbekova <zhazgulukemelbekova@gmail.com>
2021-11-16 23:51:16 +0400

    start js

---
+++ script/script.js
@@ -0,0 +1,33 @@
+const name = "Aidai"
+const city = "Bishkek"
+const age = 25
+const letter = "и"
+const word = "кот"
+const letter1 = "Я"
+const word1 = "любил"
+
+
+console.log ("my name is " + name)
+console.log ("i am "+ 25 +" years old")
+console.log ("i was born in " + city)
+console.log (city + " is very small city")
+
+console.log (letter + " днем " + letter +"  ночью "+ word +" ученый")
+
+
+console.log (letter1 + " вас "+ word1 + " A.C.Пушкин")
+console.log (letter1 + " вас " + word1 + ": любовь еще, быть может,")
+console.log (" В душе моей угасла не совсем;")
+console.log ("Но пусть она вас больше не тревожит;")
+console.log (letter1 + " не хочу печалить вас ничем.")
+console.log (letter1 + "  вас " + word1 + " безмолвно, безнадежно,")
+console.log (" То робостью, то ревностью томим;")
+console.log (letter1 + " вас " + word1 + " так искренно, так нежно,")
+console.log (" Как дай вам бог любимой быть другим.")
+
+
+
+
+
+
+
